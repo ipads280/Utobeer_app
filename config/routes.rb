d5f51865c3effda3a295003a2ce8873a8bb8efbf
ipads280 <ipads280@icloud.com>
2022-06-08 02:43:13 +0000

    いろいろ完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "user/registrations",
-  sessions: 'user/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-root to:'homes#top'
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+ 
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
   
-get 'homes/top'
-get 'homes/about',as: 'about'
+  root to:'homes#top'
+  
+  get 'homes/top'
+  get 'homes/about',as: 'about'
 end
