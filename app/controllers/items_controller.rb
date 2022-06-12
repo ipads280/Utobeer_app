0668bd497b1180368014bac77ac37f269e4e9e79
ipads280 <ipads280@icloud.com>
2022-06-12 09:21:04 +0000

    大幅完成

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,10 @@
+class ItemsController < ApplicationController
+   def index
+    @items=Item.all
+    @items = Item.page(params[:page]).per(6)
+  end
+
+  def show
+    @item=Item.find(params[:id])
+  end
+end
