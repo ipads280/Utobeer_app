6cedb30650bbb48b08c98e7e6f0096e2d1aadaf0
ipads280 <ipads280@icloud.com>
2022-06-30 04:07:23 +0000

    レビュー修正

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -2,10 +2,29 @@ class Admin::ReviewsController < ApplicationController
   before_action :authenticate_admin!
   def edit
     @review=Review.find_by(id: params[:id])
-    if @review.user==current_user
-      render 'edit'
+  end
+  
+  def update
+    item=Item.find(params[:item_id])
+    @review=Review.find_by(id: params[:id])
+    if
+      @review.update(review_params)
+      redirect_to admin_item_path(item), notice: "編集成功しました"
     else
-      redirect_to items_path
+      render :edit
     end
   end
+
+  def destroy
+    item=Item.find(params[:id])
+    review=Review.find_by(item_id: params[:id],id: params[:item_id],)
+    review.destroy
+    redirect_to admin_item_path(item), notice: "削除しました"
+  end
+  
+  private
+
+    def review_params
+      params.require(:review).permit(:item_id,:user_id,:title,:text,:star,:re)
+    end
 end
