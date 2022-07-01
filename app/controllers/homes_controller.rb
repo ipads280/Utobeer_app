431d0ba5445b0bf507f7ac479f14f4b79639c1b6
ipads280 <ipads280@icloud.com>
2022-07-01 09:56:31 +0000

    @allranksについて説明追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,14 @@ class HomesController < ApplicationController
 
   def top
    @reviews=Review.order(created_at: :desc).limit(5)
+
    @all_ranks = Item.find(Favorite.group(:item_id).order('count(item_id) desc').limit(2).pluck(:item_id))
+   #groupメソッドで、指定したカラムのデータの種類ごとにデータまとめる。
+   #つまり、 Favorie.group(:item_id)でアイテム番号が同じものでグループ分けする
+   #グループ分けしたものを番号の多い順に並び替える
+   #limit(2)で最大表示数を二つに制限する
+   #pluck(:item_id)で最後に:item_idカラムのみを数字で取り出す指定
+
    @random = Item.order("RAND()").limit(1)
   end
 
